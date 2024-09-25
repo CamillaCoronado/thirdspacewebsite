40f6538dc68c65a40df08a1bb306588ffa337928
Camilla Coronado <camilla.coronado@gmail.com>
2024-09-25 14:29:38 -0700

    design complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,33 +4,44 @@
     let waitlistLink: string = "https://jointthirdspace.com/somequ";
     let waitlistNumber: number = 432;
 
-    let showNewBlock: boolean = false;
+    let showNewBlock: string = "initial";
 
     function handleSubmit() {
     // Process the input fields
     console.log('Processing:', email, phone);
 
     // After processing, replace the block
-    showNewBlock = true;
+    showNewBlock = "waitlist success";
   }
 
   function copyToClipboard() {
     navigator.clipboard.writeText(waitlistLink);
     alert("Link copied to clipboard!"); // Optional alert
   }
+
+  function waitlistCheck() {
+    showNewBlock = "waitlist check";
+  }
+  function waitlistCheckSuccess() {
+    showNewBlock = "waitlist check success";
+  }
+
+ function backJoin() {
+  showNewBlock = "initial"
+ }
   </script>
   
   <main class="bg-indigo text-white px-16 pt-16 min-h-[100vh]">
     <!-- Navigation -->
-    <header class="flex justify-between flex-col md:flex-row items-center mb-32 md:mb-64">
+    <header class="flex justify-between flex-col md:flex-row items-center md:justify-end mb-32 md:mb-64">
       <!-- <a href="/about" class="underline text-white self-end md:self-center text-base mb-32 md:mb-0 grow md:grow-0 md:order-2">About</a> -->
-      <img src="../src/assets/logo-white.png" alt="thirdspace logo" class="w-[initial] h-[40px] md:order-1">
+      <img on:click={backJoin} src="../src/assets/logo-white.png" alt="thirdspace logo" class="w-[initial] h-[40px] md:order-1 cursor-pointer">
     </header>
   
     <!-- Main Section -->
     <section class="flex flex-col items-center">
       <div class="mb-32 flex flex-col lg:flex-row lg:justify-center items-center w-full gap-[32px] md:gap-[16px]">
-        {#if !showNewBlock} <!-- Right: Waitlist Form -->
+        {#if showNewBlock == "initial"} <!-- Right: Waitlist Form -->
         <div class="text-white rounded-lg w-full max-w-lg flex-col">
           <h4 class="text-2xl font-bold mb-8 uppercase hidden md:block">Introducing Thirdspace</h4>
           <h2 class="text-[40px] mb-16 font-bold leading-[49px]">presto chango you now have a friendo</h2>
@@ -52,9 +63,9 @@
             <button on:click={handleSubmit} class="bold-text bg-white text-indigo text-bold btn w-full rounded-full py-12">Join waitlist</button>
           </form>
           
-          <a href="/waitlist-check" class="underline text-white mt-[-8px] block text-center mb-32">Already joined? Check waitlist number.</a>
+          <button on:click={waitlistCheck} class="underline bold-text text-sm text-white mt-[-8px] block text-center mb-32">Already joined? Check waitlist number.<button/>
         </div>
-        {:else}
+        {:else if showNewBlock == "waitlist success"}
         <div class="bg-indigo w-full max-w-lg text-white rounded-lg text-left">
           <h2 class="font-bold mb-16">Yay! You're on the waitlist.</h2>
           <p class="mb-16 bold-text text-lg">Skip ahead in line by referring friends. Top 15 get one month premium free.</p>
@@ -83,6 +94,52 @@
         
           <!-- Waitlist Number -->
           <p class="text-base text-center bold-text">Waitlist Number: {waitlistNumber}</p>
+        </div>
+        {:else if showNewBlock == "waitlist check"}
+        <form class="flex flex-col max-w-lg text-dark-charcoal">
+          <p class="mb-16 bold-text text-lg text-white">Please provide the email or phone number you joined with.</p>
+          <input 
+            type="email" 
+            placeholder="Email" 
+            bind:value={email} 
+            class="mb-8 text-xl block w-full px-8 py-8 border-2 border-medium-indigo rounded-md"
+          />
+          <p class="mb-8 bold-text text-white text-lg text-center">Or</p>
+          <input 
+            type="tel" 
+            placeholder="Phone Number" 
+            bind:value={phone} 
+            class="mb-16 text-xl block w-full px-8 py-8 border-2 border-medium-indigo rounded-md"
+          />
+          <button on:click={waitlistCheckSuccess} class="bold-text bg-white text-indigo text-bold btn w-full rounded-full py-12">Check waitlist</button>
+          <button on:click={backJoin} class="text-base block underline text-white text-left mt-[-8px] bold-text">Join waitlist</button>
+        </form>
+        {:else if showNewBlock == "waitlist check success"}
+        <div class="bg-indigo w-full max-w-lg text-white rounded-lg text-left">
+          <h2 class="font-bold mb-16">Yay! You're {waitlistNumber} on the waitlist.</h2>
+          <p class="mb-16 bold-text text-lg">Skip ahead in line by referring friends. Top 15 get one month premium free.</p>
+        
+          <div class="flex justify-center items-center mb-16 space-x-2">
+            <!-- Input field -->
+            <input 
+              type="text" 
+              placeholder="https://jointthirdspace.com/somequ"
+              bind:value={waitlistLink}
+              readonly 
+              class="text-xl block w-full px-8 py-8 border-2 border-medium-indigo text-dark-charcoal rounded-md"
+            />
+            <!-- Copy button (icon) -->
+            <button on:click={copyToClipboard}>
+              <img src="../src/assets/copy-icon.png" alt="Copy" class="w-[32px] h-[34px]" />
+            </button>
+          </div>
+        
+          <!-- Social media icons -->
+          <div class="flex justify-center gap-[16px] mb-8">
+            <a href="#" class="block"><img src="../src/assets/instagram-logo.png" alt="Instagram" class="w-10 h-10" /></a>
+            <a href="#" class="block"><img src="../src/assets/twitter-logo.png" alt="X (Twitter)" class="w-10 h-10" /></a>
+            <a href="#" class="block"><img src="../src/assets/facebook-logo.png" alt="Facebook" class="w-10 h-10" /></a>
+          </div>
         </div>
         {/if}
         <div class="flex flex-row flex-wrap md:flex-row justify-center items-start mb-16 lg:mb-0">
